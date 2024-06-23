d71828aaef52e3a512f8345c4e96413a43119289
Ryan Licata <rlicata34@gmail.com>
2024-06-23 17:39:03 -0400

    javascript for profile edit modal

--- scripts/index.js
+++ scripts/index.js
@@ -30,4 +30,14 @@ const initialCards = [
   },
 ];
 
-console.log(initialCards);
+const profileEditButton = document.querySelector("#profile-edit-button");
+const profileEditModal = document.querySelector("#profile-edit-modal");
+const profileCloseButton = document.querySelector("#profile-close-button");
+
+profileEditButton.addEventListener("click", function () {
+  profileEditModal.classList.add("modal_opened");
+});
+
+profileCloseButton.addEventListener("click", function () {
+  profileEditModal.classList.remove("modal_opened");
+});
