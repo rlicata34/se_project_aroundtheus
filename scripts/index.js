c9f6683907404069a20e9ba3a9b33791b8501137
Ryan Licata <rlicata34@gmail.com>
2024-06-28 18:06:38 -0400

    activate like button feature

--- scripts/index.js
+++ scripts/index.js
@@ -76,6 +76,12 @@ function getCardElement(cardData) {
   const cardElement = cardTemplate.cloneNode(true);
   const cardImageEl = cardElement.querySelector(".card__image");
   const cardTitleEl = cardElement.querySelector(".card__title");
+  const likeButton = cardElement.querySelector(".card__like-button");
+
+  likeButton.addEventListener("click", () => {
+    likeButton.classList.toggle("card__like-button_active");
+  });
+
   cardImageEl.src = cardData.link;
   cardImageEl.alt = cardData.name;
   cardTitleEl.textContent = cardData.name;
@@ -88,10 +94,6 @@ function renderCard(cardData, cardListEl) {
 }
 
 initialCards.forEach((cardData) => renderCard(cardData, cardListEl));
-
-/* -------------------------------------------------------------------------- */
-/*                               Event Handlers                               */
-/* -------------------------------------------------------------------------- */
 
 function handleProfileEditSubmit(evt) {
   evt.preventDefault();
