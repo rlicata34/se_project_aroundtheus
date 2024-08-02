3556a3448d1ad0907a5a7a910c365e5b10fbc964
Ryan Licata <rlicata34@gmail.com>
2024-08-01 20:22:23 -0400

    changed name for input url

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -4,13 +4,13 @@ export default class PopupWithImage extends Popup {
   constructor(popupSelector) {
     super({ popupSelector });
     this._cardImageEl = this._popupEl.querySelector(".modal__image");
-    this._cardTitleEL = this._popupEl.querySelector(".modal__image-title");
+    this._cardTitleEl = this._popupEl.querySelector(".modal__image-title");
   }
 
   open(data) {
     this._cardImageEl.src = data.link;
-    this._cardImageEL.alt = data.name;
-    this._cardTitleEL.textContent = data.name;
+    this._cardImageEl.alt = data.name;
+    this._cardTitleEl.textContent = data.name;
     super.open();
   }
 }
